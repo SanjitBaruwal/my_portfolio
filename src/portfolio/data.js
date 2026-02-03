d0e572df4249e39df87b4b7e2b2f965b0d06ccb0
Sanjit Baruwal <baruwalsanjit@gmail.com>
2026-02-03 20:36:51 +0545

    portfolio updated

--- src/portfolio/data.js
+++ src/portfolio/data.js
@@ -12,76 +12,75 @@ import img10 from "../assets/admin.png";
 const data = [
   {
     id: 1,
-    image: img1,
-    title: "Todo App Save, Your Task Locally.",
-    github: "https://github.com/SanjitBaruwal/Todo-App",
-    demo: "https://savetask.netlify.app/",
-  },
-  {
-    id: 2,
-    image: img2,
-    title: "Weather App",
-    github: "https://github.com/SanjitBaruwal/React_Weather?tab=readme-ov-file",
-    demo: "#",
-  },
-  {
-    id: 3,
-    image: img3,
-    title: "Rock Scissors Paper",
-    github: "https://github.com/SanjitBaruwal/Rock_Scissors_Paper",
-    demo: "https://rockscissorpep.netlify.app",
-  },
-  {
-    id: 4,
-    image: img4,
-    title: "Keep Notes",
-    github: "https://github.com/SanjitBaruwal/Keep_Note",
-    demo: "https://keepnotes1.netlify.app",
-  },
-  {
-    id: 5,
-    image: img5,
-    title: "Hospital Management System",
-    github: "https://github.com/SanjitBaruwal/Hospital-Management-System",
-    demo: "https://hospital-management-system-patient.vercel.app",
-  },
-  {
-    id: 6,
     image: img6,
     title: "Library Management System",
     github: "https://github.com/SanjitBaruwal/Library-Management-System",
     demo: "https://library-management-system-beta-ten.vercel.app",
   },
   {
+    id: 2,
+    image: img9,
+    title: "School Web",
+    github: "#",
+    demo: "https://rbssnuwakot.edu.np",
+  },
+  {
+    id: 3,
+    image: img10,
+    title: "School Admin",
+    github: "#",
+    demo: "https://admin.rbssnuwakot.edu.np",
+  },
+  {
+    id: 4,
+    image: img1,
+    title: "Todo App Save, Your Task Locally.",
+    github: "https://github.com/SanjitBaruwal/Todo-App",
+    demo: "https://savetask.netlify.app/",
+  },
+  {
+    id: 5,
+    image: img3,
+    title: "Rock Scissors Paper",
+    github: "https://github.com/SanjitBaruwal/Rock_Scissors_Paper",
+    demo: "https://rockscissorpep.netlify.app",
+  },
+  {
+    id: 6,
+    image: img4,
+    title: "Keep Notes",
+    github: "https://github.com/SanjitBaruwal/Keep_Note",
+    demo: "https://keepnotes1.netlify.app",
+  },
+  {
     id: 7,
+    image: img5,
+    title: "Hospital Management System",
+    github: "https://github.com/SanjitBaruwal/Hospital-Management-System",
+    demo: "https://hospital-management-system-patient.vercel.app",
+  },
+  {
+    id: 8,
+    image: img2,
+    title: "Weather App",
+    github: "https://github.com/SanjitBaruwal/React_Weather?tab=readme-ov-file",
+    demo: "#",
+  },
+
+  {
+    id: 9,
     image: img7,
     title: "Nepal Address Api",
     github: "https://github.com/SanjitBaruwal/Nepal_Location_API",
     demo: "https://nepal-location-api-client.vercel.app",
   },
   {
-    id: 8,
+    id: 10,
     image: img8,
     title: "Number Plate Detection System",
     github:
       "https://github.com/rajeevy397/Nepali_Vehicle_License_Plate_Detector_And_Reader_YOLOv8",
     demo: "#",
-  },
-   {
-    id: 9,
-    image: img9,
-    title: "School Web",
-    github:
-      "#",
-    demo: "https://rbssnuwakot.edu.np",
-  },
-   {
-    id: 10,
-    image: img10,
-    title: "School Admin",
-    github:
-      "#",
-    demo: "https://admin.rbssnuwakot.edu.np",
   },
 ];
 
